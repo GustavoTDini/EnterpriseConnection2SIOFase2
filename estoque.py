85aa3f740dd9e5758f5a76256da7eccaa245d2b8
Gustavo T Dini <gustavotdini@outlook.com>
2022-04-11 19:11:54 -0300

    Finish Stock Otimization

--- estoque.py
+++ estoque.py
@@ -1,17 +1,23 @@
-from produto import produto
+from produto import Produto
 
 
-class estoque:
+# Classe para o controle do nosso estoque, baseado em uma fila
+class Estoque:
+    # Definição do estoque e indicando quanto será o estoque a ser mantido
     def __init__(self, estoque_total):
         self.estoque = []
         self.estoque_total = estoque_total
 
-    def estocar(self, produto):
-        self.estoque.append(produto)
+    # função para colocar um novo produto no estoque
+    def estocar(self, novo_produto):
+        self.estoque.append(novo_produto)
 
+    # função para selecionar um produto do estoque
     def pegar_produto(self, index):
         return self.estoque[index]
 
+    # função que realiza uma venda, retira o produto mais antigo do estoque e calcula seu lucro,
+    # considerando o custo deste
     def venda(self):
         lucro = 0
         if not self.sem_estoque():
@@ -19,22 +25,26 @@ class estoque:
             self.estoque.pop(0)
         return lucro
 
+    # função que verifica se o estoque está vazio
     def sem_estoque(self):
         return not self.estoque
 
+    # função que retorna a quantidade de produtos no estoque
     def quantidade_estoque(self):
         return len(self.estoque)
 
-    def restocar(self, valor_venda, custo_compra):
+    # função que re-estoca até o total definido considerando o valor de compra do mês, retorna o custo desta compra
+    def re_estocar(self, valor_venda, custo_compra):
         quantidade_a_comprar = 0
         if self.quantidade_estoque() < self.estoque_total:
             quantidade_a_comprar = self.estoque_total - self.quantidade_estoque()
         for i in range(quantidade_a_comprar):
-            self.estocar(produto(valor_venda, custo_compra))
-        return custo_compra*quantidade_a_comprar
+            self.estocar(Produto(valor_venda, custo_compra))
+        return custo_compra * quantidade_a_comprar
 
+    # função para determinar o custo total do atual estoque, considerando os diversos valores de compra
     def custo_estoque(self):
         valor_estoque = 0
-        for produto in self.estoque:
-            valor_estoque += produto.valor_de_compra()
+        for produto_estocado in self.estoque:
+            valor_estoque += produto_estocado.valor_de_compra()
         return valor_estoque
--- produto.py
+++ produto.py
@@ -1,13 +1,18 @@
-class produto:
+# Classe que contem as informações de um produto
+class Produto:
+    # Definição do produto, considera o valor para vendas e custo de compras
     def __init__(self, valor_venda, custo_compra):
         self.valor_venda = valor_venda
         self.custo_compra = custo_compra
 
+    # Função que retorna o valor de venda do produto
     def valor_de_venda(self):
         return self.valor_venda
 
+    # Função que retorna o custo de compra do produto
     def valor_de_compra(self):
         return self.custo_compra
 
+    # Função que retorna o lucro do produto, por quanto ele foi vendido menos quanto custou
     def lucro(self):
         return self.valor_venda - self.custo_compra
